b435f632259d53594391a69d7012ac83fef8f048
Michail Bousios <mimpous@gmail.com>
2025-01-24 14:20:33 +0100

    adding swagger support

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # docker-demo
+ 
+
+docker build -f Dockerfile -t mimpous/consid-demo:latest --push .
+
+docker pull mimpous/consid-demo
+docker run 8080:8080 mimpous/consid-demo
--- src/main/java/com/consid/demo/TestController.java
+++ src/main/java/com/consid/demo/TestController.java
@@ -5,26 +5,34 @@ import java.net.UnknownHostException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
-@CrossOrigin(origins = "*") 
+@RequestMapping("/api/v1")
+@Tag(name = "User API", description = "API for managing users")
 public class TestController {
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String demo() {
 		return "application name : Consid Demo Project";
 	}
-	@RequestMapping("/api/test")
+	@GetMapping("/test")
+    @Operation(summary = "Get test ", description = "Retrieve demo-test string.")
+
 	public String test() {
-		return "application name : this is a test";
+		return "application : demo-test";
 	}
 	
 	
-	@RequestMapping("/api/{name}")
+	@GetMapping("/{name}")
+    @Operation(summary = "Retrieves a json", description = "Retrieves a host name and ip and name as input.")
+
 	public ResponseEntity<ResponseOut>  demoWithParameters( @PathVariable(name = "name", required = true ) String name ) {
 		
 		String hostName = "";
@@ -40,6 +48,7 @@ public class TestController {
 		return  new ResponseEntity<>( new ResponseOut( hostName, hostIp, name), HttpStatus.OK);
 		
 	}
+ 
 	
 	
 }
